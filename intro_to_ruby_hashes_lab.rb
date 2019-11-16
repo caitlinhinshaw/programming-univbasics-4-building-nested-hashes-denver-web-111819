0fa9fcf7a874ec8a0d248d4a958094357b9ed7a8
Caitlin Hinshaw <caitlin.hinshaw@gmail.com>
2019-11-16 06:51:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,7 +33,7 @@ def monopoly_with_third_tier
         reading_railroad: {},
         b_and_o_railroad: {},
         pennsylvania_railroad: {},
-        shortline: {}
+        shortline_railroad: {}
         }
       }
     }
